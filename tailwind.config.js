3cb5f2846f551cb963ea7e029397a3c11be8e6d5
TopETH <supernathanliu@gmail.com>
2022-02-06 14:35:05 -0500

    feat: initial setup for nextjs+typescript+tailwindcss+react-icons

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'light-green': '#76FBE3',
+        'light-purple': '#FF53ED',
+        'dark-grey': '#150F23',
+      },
+    },
+  },
+  plugins: [],
+}
